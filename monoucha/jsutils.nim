83abe9815601a3fd5e697a4941a426c56f54c889
bptato <nincsnevem662@gmail.com>
2024-07-28 20:34:35 +0200

    jsutils: fix toJSValueArray on 0-len arrays

--- monoucha/jsutils.nim
+++ monoucha/jsutils.nim
@@ -1,7 +1,10 @@
 import quickjs
 
 template toJSValueArray*(a: openArray[JSValue]): ptr UncheckedArray[JSValue] =
-  cast[ptr UncheckedArray[JSValue]](unsafeAddr a[0])
+  if a.len > 0:
+    cast[ptr UncheckedArray[JSValue]](unsafeAddr a[0])
+  else:
+    nil
 
 # Warning: this must be a template, because we're taking the address of
 # the passed value, and Nim is pass-by-value.
