3b3b2a9afd41c64e45c3ab59afd6b0bb49458173
bptato <nincsnevem662@gmail.com>
2024-09-17 22:01:14 +0200

    Update monoucha
    I meant to fix this enum bug in the previous patch but then I forgot...

--- src/version.nim
+++ src/version.nim
@@ -29,4 +29,4 @@ tryImport monoucha/version, "monoucha"
 static:
   checkVersion("chagashi", 0, 5, 6)
   checkVersion("chame", 1, 0, 1)
-  checkVersion("monoucha", 0, 4, 2)
+  checkVersion("monoucha", 0, 4, 3)
