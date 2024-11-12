sourcehut-mirrors/chawan

commit 23beebe67a445aaeeae20a5cd80ff522db1b0ddb

url: reduce URL object size, simplify
* query, fragment are now strings, not options
* get rid of Host, Path types, now hostname/pathname are strings
* fix ipv4 parsing error case
* in file protocol, fix URL for dirlist without slash with a redirection

src/local/container.nim
@@ -1427,7 +1427,7 @@ proc onload(container: Container; res: int) =
     container.setLoadInfo("")
     container.triggerEvent(cetStatus)
     container.triggerEvent(cetLoaded)
-    if cfHasStart notin container.flags and (container.url.anchor != "" or
+    if cfHasStart notin container.flags and (container.url.hash != "" or
         container.config.autofocus):
       container.requestLines().then(proc(): Promise[GotoAnchorResult] =
         return container.iface.gotoAnchor()

src/local/pager.nim
@@ -1390,7 +1390,8 @@ proc gotoURL(pager: Pager; request: Request; prevurl = none(URL);
     if ourl == nil:
       break
     request.url = ourl
-  if prevurl.isNone or not prevurl.get.equals(request.url, true) or
+  if prevurl.isNone or
+      not prevurl.get.equals(request.url, excludeHash = true) or
       request.url.hash == "" or request.httpMethod != hmGet or save:
     # Basically, we want to reload the page *only* when
     # a) we force a reload (by setting prevurl to none)
@@ -1428,7 +1429,7 @@ proc gotoURL(pager: Pager; request: Request; prevurl = none(URL);
     inc pager.numload
     return container
   else:
-    pager.container.findAnchor(request.url.anchor)
+    pager.container.findAnchor(request.url.hash)
     return nil
 
 proc omniRewrite(pager: Pager; s: string): string =

src/server/buffer.nim
@@ -707,7 +707,7 @@ type GotoAnchorResult* = object
 proc gotoAnchor*(buffer: Buffer): GotoAnchorResult {.proxy.} =
   if buffer.document == nil:
     return GotoAnchorResult(found: false)
-  var anchor = buffer.document.findAnchor(buffer.url.anchor)
+  var anchor = buffer.document.findAnchor(buffer.url.hash)
   var focus: ReadLineResult = nil
   if buffer.config.autofocus:
     let autofocus = buffer.document.findAutoFocus()
@@ -1232,7 +1232,7 @@ proc makeFormRequest(buffer: Buffer; parsedAction: URL; httpMethod: HttpMethod;
       # mutate action URL
       let kvlist = entryList.toNameValuePairs()
       #TODO with charset
-      parsedAction.query = some(serializeFormURLEncoded(kvlist))
+      parsedAction.setSearch(serializeFormURLEncoded(kvlist, qmark = true))
       return newRequest(parsedAction, httpMethod)
     return newRequest(parsedAction) # get action URL
   of frtMailto:
@@ -1240,8 +1240,8 @@ proc makeFormRequest(buffer: Buffer; parsedAction: URL; httpMethod: HttpMethod;
       # mailWithHeaders
       let kvlist = entryList.toNameValuePairs()
       #TODO with charset
-      let headers = serializeFormURLEncoded(kvlist, spaceAsPlus = false)
-      parsedAction.query = some(headers)
+      parsedAction.setSearch(serializeFormURLEncoded(kvlist,
+        spaceAsPlus = false, qmark = true))
       return newRequest(parsedAction, httpMethod)
     # mail as body
     let kvlist = entryList.toNameValuePairs()
@@ -1250,19 +1250,19 @@ proc makeFormRequest(buffer: Buffer; parsedAction: URL; httpMethod: HttpMethod;
     else:
       #TODO with charset
       serializeFormURLEncoded(kvlist)
-    if parsedAction.query.isNone:
-      parsedAction.query = some("")
-    if parsedAction.query.get != "":
-      parsedAction.query.get &= '&'
-    parsedAction.query.get &= "body=" & body
+    if parsedAction.search == "":
+      parsedAction.search = "?"
+    if parsedAction.search != "?":
+      parsedAction.search &= '&'
+    parsedAction.search &= "body=" & body
     return newRequest(parsedAction, httpMethod)
   of frtHttp:
     if httpMethod == hmGet:
       # mutate action URL
       let kvlist = entryList.toNameValuePairs()
       #TODO with charset
-      let query = serializeFormURLEncoded(kvlist)
-      parsedAction.query = some(query)
+      let query = serializeFormURLEncoded(kvlist, qmark = true)
+      parsedAction.setSearch(query)
       return newRequest(parsedAction, httpMethod)
     # submit as entity body
     let body = case enctype

adapter/protocol/file.nim
@@ -79,7 +79,10 @@ proc main() =
   if f.open(path, fmRead):
     loadFile(f)
   elif dirExists(path):
-    loadDir(path, opath)
+    if path[^1] != '/':
+      stdout.write("Status: 301\nLocation: " & path & "/\n")
+    else:
+      loadDir(path, opath)
   else:
     stdout.write("Cha-Control: ConnectionError FileNotFound")
 

src/loader/loader.nim
@@ -502,8 +502,8 @@ proc putMappedURL(url: URL) =
   putEnv("MAPPED_URI_PASSWORD", url.password)
   putEnv("MAPPED_URI_HOST", url.hostname)
   putEnv("MAPPED_URI_PORT", url.port)
-  putEnv("MAPPED_URI_PATH", url.path.serialize())
-  putEnv("MAPPED_URI_QUERY", url.query.get(""))
+  putEnv("MAPPED_URI_PATH", url.pathname)
+  putEnv("MAPPED_URI_QUERY", url.search.substr(1))
 
 type CGIPath = object
   basename: string
@@ -528,8 +528,8 @@ proc setupEnv(cpath: CGIPath; request: Request; contentLen: int; prevURL: URL;
     putMappedURL(prevURL)
   if cpath.pathInfo != "":
     putEnv("PATH_INFO", cpath.pathInfo)
-  if url.query.isSome:
-    putEnv("QUERY_STRING", url.query.get)
+  if url.search != "":
+    putEnv("QUERY_STRING", url.search.substr(1))
   if request.httpMethod == hmPost:
     if request.body.t == rbtMultipart:
       putEnv("CONTENT_TYPE", request.body.multipart.getContentType())
@@ -754,7 +754,7 @@ proc loadStream(ctx: LoaderContext; client: ClientData; handle: InputHandle;
 proc loadFromCache(ctx: LoaderContext; client: ClientData; handle: InputHandle;
     request: Request) =
   let id = parseInt32(request.url.pathname).get(-1)
-  let startFrom = parseInt32(request.url.query.get("")).get(0)
+  let startFrom = parseInt32(request.url.search.substr(1)).get(0)
   let (ps, n) = client.openCachedItem(id)
   if ps != nil:
     if startFrom != 0:
@@ -805,13 +805,13 @@ proc loadDataSend(ctx: LoaderContext; handle: InputHandle; s, ct: string) =
 
 proc loadData(ctx: LoaderContext; handle: InputHandle; request: Request) =
   let url = request.url
-  var ct = url.path.s.until(',')
+  var ct = url.pathname.until(',')
   if AllChars - Ascii + Controls - {'\t', ' '} in ct:
     handle.sendResult(ceInvalidURL, "invalid data URL")
     handle.close()
     return
   let sd = ct.len + 1 # data start
-  let body = percentDecode(url.path.s.toOpenArray(sd, url.path.s.high))
+  let body = percentDecode(url.pathname.toOpenArray(sd, url.pathname.high))
   if ct.endsWith(";base64"):
     let d = atob0(body) # decode from ct end + 1
     if d.isNone:
@@ -831,7 +831,7 @@ proc loadResource(ctx: LoaderContext; client: ClientData;
   while redo and tries < MaxRewrites:
     redo = false
     if ctx.config.w3mCGICompat and request.url.scheme == "file":
-      let path = request.url.path.serialize_unicode()
+      let path = request.url.pathname.percentDecode()
       if ctx.canRewriteForCGICompat(path):
         let newURL = newURL("cgi-bin:" & path & request.url.search)
         if newURL.isSome:

src/local/client.nim
@@ -645,7 +645,7 @@ proc clientLoadJSModule(ctx: JSContext; module_name: cstringConst;
     JS_ThrowTypeError(ctx, "Invalid URL: %s", module_name)
     return nil
   try:
-    let f = readFile($x.get.path)
+    let f = readFile(x.get.pathname)
     return finishLoadModule(ctx, f, cstring(module_name))
   except IOError:
     JS_ThrowTypeError(ctx, "Failed to open file %s", module_name)

src/types/cookie.nim
@@ -125,7 +125,7 @@ proc `$`*(cookiejar: CookieJar): string =
 
 # https://www.rfc-editor.org/rfc/rfc6265#section-5.1.4
 func defaultCookiePath(url: URL): string =
-  let path = ($url.path).beforeLast('/')
+  let path = url.pathname.beforeLast('/')
   if path == "" or path[0] != '/':
     return "/"
   return path
@@ -197,7 +197,7 @@ proc serialize*(cookiejar: CookieJar; url: URL): string =
       continue
     if cookie.secure and url.scheme != "https":
       continue
-    if not cookiePathMatches(cookie.path, $url.path):
+    if not cookiePathMatches(cookie.path, url.pathname):
       continue
     if not cookieDomainMatches(cookie.domain, url):
       continue

commit a4133432e47d9e45159f0ed1d4e7f6a2c02dc5b6

buffer: fix broken gotoAnchor behavior
23beebe6 introduced a regression that broke gotoAnchor. This fixes that,
plus a couple other long-standing gotoAnchor bugs:

* If no anchor is found, do not dupe the buffer.
  Desktop browsers still add a history entry, while w3m prints an
  error. I've copied the latter because it makes more sense as a user,
  but this will have to be refined for the navigation API at some point.
* If the anchor *is* found, then always jump to it, even if it's not
  visible.
  This was a limitation of relying on the line array, so now we rely on
  the box tree instead. (Sooner or later, the former must go anyway.)

Also, fix `U' reload not restoring the position (hopefully this time for
good).

src/css/box.nim
@@ -58,6 +58,7 @@ type
 
   InlineFragment* = ref object
     state*: InlineFragmentState
+    render*: BoxRenderState
     computed*: CSSComputedValues
     node*: StyledNode
     splitType*: set[SplitType]
@@ -79,8 +80,12 @@ type
 
   RelativeRect* = array[DimensionType, Span]
 
+  BoxRenderState* = object
+    offset*: Offset
+
   BlockBox* = ref object
     state*: BoxLayoutState
+    render*: BoxRenderState
     computed*: CSSComputedValues
     node*: StyledNode
     inline*: InlineFragment

src/css/render.nim
@@ -2,7 +2,6 @@ import std/algorithm
 
 import css/box
 import css/cssvalues
-import css/layout
 import css/lunit
 import css/stylednode
 import types/bitmap
@@ -345,8 +344,10 @@ proc renderInlineFragment(grid: var FlexibleGrid; state: var RenderState;
     if bgcolor0.a > 0:
       grid.paintInlineFragment(state, fragment, offset,
         bgcolor0.rgb.cellColor())
+  let startOffset = offset + fragment.state.startOffset
+  fragment.render.offset = startOffset
   if position notin PositionStaticLike and stSplitStart in fragment.splitType:
-    state.absolutePos.add(offset + fragment.state.startOffset)
+    state.absolutePos.add(startOffset)
   if fragment.t == iftParent:
     for child in fragment.children:
       grid.renderInlineFragment(state, child, offset, bgcolor0)
@@ -396,6 +397,7 @@ proc renderBlockBox(grid: var FlexibleGrid; state: var RenderState;
     if not box.computed{"top"}.auto or not box.computed{"bottom"}.auto:
       offset.y = state.absolutePos[^1].y
   offset += box.state.offset
+  box.render.offset = offset
   if position notin PositionStaticLike:
     state.absolutePos.add(offset)
   if box.computed{"visibility"} == VisibilityVisible:
@@ -438,13 +440,12 @@ proc renderBlockBox(grid: var FlexibleGrid; state: var RenderState;
     discard state.absolutePos.pop()
 
 proc renderDocument*(grid: var FlexibleGrid; bgcolor: var CellColor;
-    styledRoot: StyledNode; attrsp: ptr WindowAttributes;
+    rootBox: BlockBox; attrsp: ptr WindowAttributes;
     images: var seq[PosBitmap]) =
   grid.setLen(0)
-  if styledRoot == nil:
+  if rootBox == nil:
     # no HTML element when we run cascade; just clear all lines.
     return
-  let rootBox = styledRoot.layout(attrsp)
   var state = RenderState(
     absolutePos: @[offset(0, 0)],
     attrsp: attrsp,

src/local/container.nim
@@ -46,9 +46,8 @@ type
     setxsave: bool
 
   ContainerEventType* = enum
-    cetAnchor, cetNoAnchor, cetReadLine, cetReadArea, cetReadFile, cetOpen,
-    cetSetLoadInfo, cetStatus, cetAlert, cetLoaded, cetTitle, cetCancel,
-    cetMetaRefresh
+    cetReadLine, cetReadArea, cetReadFile, cetOpen, cetSetLoadInfo, cetStatus,
+    cetAlert, cetLoaded, cetTitle, cetCancel, cetMetaRefresh
 
   ContainerEvent* = object
     case t*: ContainerEventType
@@ -62,8 +61,6 @@ type
       request*: Request
       save*: bool
       url*: URL
-    of cetAnchor, cetNoAnchor:
-      anchor*: string
     of cetAlert:
       msg*: string
     of cetMetaRefresh:
@@ -151,7 +148,7 @@ type
     # if set, this *overrides* any content type received from the network. (this
     # is because it stores the content type from the -T flag.)
     contentType* {.jsget.}: Option[string]
-    pos: CursorPosition
+    pos*: CursorPosition
     bpos: seq[CursorPosition]
     highlights: seq[Highlight]
     process* {.jsget.}: int
@@ -489,13 +486,16 @@ proc triggerEvent(container: Container; event: ContainerEvent) =
 proc triggerEvent(container: Container; t: ContainerEventType) =
   container.triggerEvent(ContainerEvent(t: t))
 
-proc setNumLines(container: Container; lines: int; finish = false) =
+proc gotoStart(container: Container) =
+  container.pos = container.startpos.get
+  container.startpos = none(CursorPosition)
+  container.needslines = true
+
+proc setNumLines(container: Container; lines: int) =
   if container.numLines != lines:
     container.numLines = lines
-    if container.startpos.isSome and finish:
-      container.pos = container.startpos.get
-      container.startpos = none(CursorPosition)
-      container.needslines = true
+    if container.startpos.isSome and lines >= container.startpos.get.cursory:
+      container.gotoStart()
     container.updateCursor()
 
 proc queueDraw*(container: Container) =
@@ -514,7 +514,7 @@ proc requestLines(container: Container): EmptyPromise {.discardable.} =
     if isBgNew:
       container.bgcolor = res.bgcolor
     if res.numLines != container.numLines:
-      container.setNumLines(res.numLines, true)
+      container.setNumLines(res.numLines)
       if container.loadState != lsLoading:
         container.triggerEvent(cetStatus)
     if res.numLines > 0:
@@ -706,7 +706,7 @@ proc setCursorYCenter(container: Container; y: int; refresh = true)
   if fy != container.fromy:
     container.centerLine()
 
-proc setCursorXYCenter(container: Container; x, y: int; refresh = true)
+proc setCursorXYCenter*(container: Container; x, y: int; refresh = true)
     {.jsfunc.} =
   let fy = container.fromy
   let fx = container.fromx
@@ -1102,9 +1102,12 @@ proc popCursorPos*(container: Container; nojump = false) =
       container.sendCursorPosition()
       container.needslines = true
 
-proc copyCursorPos*(container, c2: Container) =
-  container.startpos = some(c2.pos)
+proc setStartingPos*(container: Container; pos: CursorPosition) =
+  container.startpos = some(pos)
   container.flags.incl(cfHasStart)
+
+proc setStartingPos*(container: Container; x, y: int) =
+  container.setStartingPos(CursorPosition(setx: -1, cursorx: x, cursory: y))
 
 proc cursorNextLink*(container: Container; n = 1) {.jsfunc.} =
   if container.iface == nil:
@@ -1427,18 +1430,21 @@ proc onload(container: Container; res: int) =
     container.setLoadInfo("")
     container.triggerEvent(cetStatus)
     container.triggerEvent(cetLoaded)
-    if cfHasStart notin container.flags and (container.url.hash != "" or
-        container.config.autofocus):
-      container.requestLines().then(proc(): Promise[GotoAnchorResult] =
-        return container.iface.gotoAnchor()
-      ).then(proc(res: GotoAnchorResult) =
-        if res.found:
-          container.setCursorXYCenter(res.x, res.y)
-          if res.focus != nil:
-            container.onReadLine(res.focus)
-      )
+    if cfHasStart in container.flags:
+      if container.startpos.isSome:
+        container.gotoStart()
     else:
-      container.needslines = true
+      let anchor = container.url.hash.substr(1)
+      if anchor != "" or container.config.autofocus:
+        container.requestLines().then(proc(): Promise[GotoAnchorResult] =
+          return container.iface.gotoAnchor(anchor, container.config.autofocus)
+        ).then(proc(res: GotoAnchorResult) =
+          if res.found:
+            container.setCursorXYCenter(res.x, res.y)
+            if res.focus != nil:
+              container.onReadLine(res.focus)
+        )
+    container.needslines = true
     if container.config.metaRefresh != mrNever:
       container.iface.checkRefresh().then(proc(res: CheckRefreshResult) =
         if res.n >= 0:
@@ -1519,14 +1525,6 @@ proc cancel*(container: Container) {.jsfunc.} =
       container.remoteCancel()
     else:
       container.triggerEvent(cetCancel)
-
-proc findAnchor*(container: Container; anchor: string) =
-  container.iface.findAnchor(anchor).then(proc(found: bool) =
-    if found:
-      container.triggerEvent(ContainerEvent(t: cetAnchor, anchor: anchor))
-    else:
-      container.triggerEvent(ContainerEvent(t: cetNoAnchor, anchor: anchor))
-  )
 
 proc readCanceled*(container: Container) =
   container.iface.readCanceled().then(proc(repaint: bool) =
@@ -1694,7 +1692,7 @@ proc onreadline(container: Container; w: Slice[int];
     container.iface.getLines(w).then(proc(res: GetLinesResult) =
       container.onreadline(w, handle, res))
   else:
-    container.setNumLines(res.numLines, true)
+    container.setNumLines(res.numLines)
 
 # Synchronously read all lines in the buffer.
 proc readLines*(container: Container; handle: proc(line: SimpleFlexibleLine)) =

src/local/pager.nim
@@ -865,7 +865,7 @@ proc dupeBuffer(pager: Pager; container: Container; url: URL) =
   if p == nil:
     pager.alert("Failed to duplicate buffer.")
   else:
-    p.then(proc(container: Container) =
+    p.then(proc(container: Container): Container =
       if container == nil:
         pager.alert("Failed to duplicate buffer.")
       else:
@@ -1423,13 +1423,21 @@ proc gotoURL(pager: Pager; request: Request; prevurl = none(URL);
       else:
         container.replaceBackup = replaceBackup
         replaceBackup.replaceRef = container
-      container.copyCursorPos(replace)
+      container.setStartingPos(replace.pos)
     else:
       pager.addContainer(container)
     inc pager.numload
     return container
   else:
-    pager.container.findAnchor(request.url.hash)
+    let container = pager.container
+    let url = request.url
+    let anchor = url.hash.substr(1)
+    container.iface.gotoAnchor(anchor, false).then(proc(res: GotoAnchorResult) =
+      if res.found:
+        pager.dupeBuffer(container, url)
+      else:
+        pager.alert("Anchor " & url.hash & " not found")
+    )
     return nil
 
 proc omniRewrite(pager: Pager; s: string): string =
@@ -2231,12 +2239,6 @@ proc handleEvent0(pager: Pager; container: Container; event: ContainerEvent):
   case event.t
   of cetLoaded:
     dec pager.numload
-  of cetAnchor:
-    let url2 = newURL(container.url)
-    url2.setHash(event.anchor)
-    pager.dupeBuffer(container, url2)
-  of cetNoAnchor:
-    pager.alert("Couldn't find anchor " & event.anchor)
   of cetReadLine:
     if container == pager.container:
       pager.setLineEdit(lmBuffer, event.value, hide = event.password,

src/server/buffer.nim
@@ -11,9 +11,12 @@ import chagashi/decoder
 import chagashi/decodercore
 import chame/tags
 import config/config
+import css/box
 import css/cascade
 import css/cssparser
 import css/cssvalues
+import css/layout
+import css/lunit
 import css/render
 import css/sheet
 import css/stylednode
@@ -51,12 +54,11 @@ import utils/twtstr
 
 type
   BufferCommand* = enum
-    bcLoad, bcForceRender, bcWindowChange, bcFindAnchor, bcReadSuccess,
-    bcReadCanceled, bcClick, bcFindNextLink, bcFindPrevLink, bcFindNthLink,
-    bcFindRevNthLink, bcFindNextMatch, bcFindPrevMatch, bcGetLines,
-    bcUpdateHover, bcGotoAnchor, bcCancel, bcGetTitle, bcSelect, bcClone,
-    bcFindPrevParagraph, bcFindNextParagraph, bcMarkURL, bcToggleImages,
-    bcCheckRefresh
+    bcLoad, bcForceRender, bcWindowChange, bcReadSuccess, bcReadCanceled,
+    bcClick, bcFindNextLink, bcFindPrevLink, bcFindNthLink, bcFindRevNthLink,
+    bcFindNextMatch, bcFindPrevMatch, bcGetLines, bcUpdateHover, bcGotoAnchor,
+    bcCancel, bcGetTitle, bcSelect, bcClone, bcFindPrevParagraph,
+    bcFindNextParagraph, bcMarkURL, bcToggleImages, bcCheckRefresh
 
   BufferState = enum
     bsLoadingPage, bsLoadingResources, bsLoaded
@@ -98,6 +100,7 @@ type
     pollData: PollData
     prevStyled: StyledNode
     prevnode: StyledNode
+    rootBox: BlockBox
     pstream: SocketStream # control stream
     quirkstyle: CSSStylesheet
     reportedBytesRead: int
@@ -463,12 +466,12 @@ proc findPrevLink*(buffer: Buffer; cursorx, cursory, n: int):
 
   var ly = 0 #last y
   var lx = 0 #last x
-  template link_beginning() =
-    #go to beginning of link
+  template link_beginning(y: int) =
+    # go to beginning of link
     ly = y #last y
     lx = format.pos #last x
 
-    #on the current line
+    # on the current line
     let line = buffer.lines[y]
     while i >= 0:
       let format = line.formats[i]
@@ -477,7 +480,7 @@ proc findPrevLink*(buffer: Buffer; cursorx, cursory, n: int):
         lx = format.pos
       dec i
 
-    #on previous lines
+    # on previous lines
     for iy in countdown(ly - 1, 0):
       let line = buffer.lines[iy]
       i = line.formats.len - 1
@@ -507,8 +510,7 @@ proc findPrevLink*(buffer: Buffer; cursorx, cursory, n: int):
     let format = line.formats[i]
     let fl = format.node.getClickable()
     if fl != nil and fl != link:
-      let y = cursory
-      link_beginning
+      link_beginning cursory
       found_pos lx, ly, fl
     dec i
 
@@ -519,7 +521,7 @@ proc findPrevLink*(buffer: Buffer; cursorx, cursory, n: int):
       let format = line.formats[i]
       let fl = format.node.getClickable()
       if fl != nil and fl != link:
-        link_beginning
+        link_beginning y
         found_pos lx, ly, fl
       dec i
   return (-1, -1)
@@ -704,12 +706,44 @@ type GotoAnchorResult* = object
   y*: int
   focus*: ReadLineResult
 
-proc gotoAnchor*(buffer: Buffer): GotoAnchorResult {.proxy.} =
+proc findAnchor(box: BlockBox; anchor: Element): Offset
+
+proc findAnchor(fragment: InlineFragment; anchor: Element): Offset =
+  if fragment.t == iftBox:
+    let off = fragment.box.findAnchor(anchor)
+    if off.y >= 0:
+      return off
+  elif fragment.t == iftParent:
+    for child in fragment.children:
+      let off = child.findAnchor(anchor)
+      if off.y >= 0:
+        return off
+  if fragment.node != nil and fragment.node.node == anchor:
+    return fragment.render.offset
+  return offset(-1, -1)
+
+proc findAnchor(box: BlockBox; anchor: Element): Offset =
+  if box.inline != nil:
+    let off = box.inline.findAnchor(anchor)
+    if off.y >= 0:
+      return off
+  for child in box.children:
+    let off = child.findAnchor(anchor)
+    if off.y >= 0:
+      return off
+  if box.node != nil and box.node.node == anchor:
+    return box.render.offset
+  return offset(-1, -1)
+
+proc gotoAnchor*(buffer: Buffer; anchor: string; autofocus: bool):
+    GotoAnchorResult {.proxy.} =
   if buffer.document == nil:
     return GotoAnchorResult(found: false)
-  var anchor = buffer.document.findAnchor(buffer.url.hash)
+  var anchor = buffer.document.findAnchor(anchor)
   var focus: ReadLineResult = nil
-  if buffer.config.autofocus:
+  # Do not use buffer.config.autofocus when we just want to check if the
+  # anchor can be found.
+  if autofocus:
     let autofocus = buffer.document.findAutoFocus()
     if autofocus != nil:
       if anchor == nil:
@@ -718,18 +752,10 @@ proc gotoAnchor*(buffer: Buffer): GotoAnchorResult {.proxy.} =
       focus = res.readline.get(nil)
   if anchor == nil:
     return GotoAnchorResult(found: false)
-  for y in 0 ..< buffer.lines.len:
-    let line = buffer.lines[y]
-    for i in 0 ..< line.formats.len:
-      let format = line.formats[i]
-      if format.node != nil and format.node.node in anchor:
-        return GotoAnchorResult(
-          found: true,
-          x: format.pos,
-          y: y,
-          focus: focus
-        )
-  return GotoAnchorResult(found: false)
+  let offset = buffer.rootBox.findAnchor(anchor)
+  let x = max(offset.x div buffer.attrs.ppc, 0).toInt
+  let y = max(offset.y div buffer.attrs.ppl, 0).toInt
+  return GotoAnchorResult(found: true, x: x, y: y, focus: focus)
 
 type CheckRefreshResult* = object
   # n is timeout in millis. -1 => not found
@@ -788,8 +814,12 @@ proc reshape(buffer: Buffer) =
     buffer.prevStyled = nil
   let styledRoot = buffer.document.applyStylesheets(uastyle,
     buffer.userstyle, buffer.prevStyled)
-  buffer.lines.renderDocument(buffer.bgcolor, styledRoot, addr buffer.attrs,
-    buffer.images)
+  # applyStylesheets may return nil if there is no <html> element.
+  buffer.rootBox = nil
+  if styledRoot != nil:
+    buffer.rootBox = styledRoot.layout(addr buffer.attrs)
+  buffer.lines.renderDocument(buffer.bgcolor, buffer.rootBox,
+    addr buffer.attrs, buffer.images)
   buffer.prevStyled = styledRoot
 
 proc maybeReshape(buffer: Buffer) =
@@ -1088,9 +1118,9 @@ proc hasTask(buffer: Buffer; cmd: BufferCommand): bool =
 proc resolveTask[T](buffer: Buffer; cmd: BufferCommand; res: T) =
   let packetid = buffer.tasks[cmd]
   assert packetid != 0
-  buffer.pstream.withPacketWriter w:
-    w.swrite(packetid)
-    w.swrite(res)
+  buffer.pstream.withPacketWriter wt:
+    wt.swrite(packetid)
+    wt.swrite(res)
   buffer.tasks[cmd] = 0
 
 proc onload(buffer: Buffer) =
@@ -1610,9 +1640,6 @@ proc select*(buffer: Buffer; selected: seq[int]): ClickResult {.proxy.} =
 proc readCanceled*(buffer: Buffer): bool {.proxy.} =
   return buffer.restoreFocus()
 
-proc findAnchor*(buffer: Buffer; anchor: string): bool {.proxy.} =
-  return buffer.document != nil and buffer.document.findAnchor(anchor) != nil
-
 type GetLinesResult* = tuple
   numLines: int
   lines: seq[SimpleFlexibleLine]
@@ -1741,14 +1768,14 @@ macro bufferDispatcher(funs: static ProxyMap; buffer: Buffer;
     var resolve = newStmtList()
     if rval == nil:
       resolve.add(quote do:
-        buffer.pstream.withPacketWriter w:
-          w.swrite(`packetid`)
+        buffer.pstream.withPacketWriter wt:
+          wt.swrite(`packetid`)
       )
     else:
       resolve.add(quote do:
-        buffer.pstream.withPacketWriter w:
-          w.swrite(`packetid`)
-          w.swrite(`rval`)
+        buffer.pstream.withPacketWriter wt:
+          wt.swrite(`packetid`)
+          wt.swrite(`rval`)
       )
     if v.istask:
       let en = v.ename