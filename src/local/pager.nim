1e86dfba0f50ec3e23ea781b44c81db4a7878560
bptato <nincsnevem662@gmail.com>
2024-12-10 17:07:58 +0100

    pager: fix nil deref
    isearch feels quite janky in general, and I think there's still a race
    lurking here...  for now it's ok, but like buffer display, this really
    belongs in a state machine (not promises)

--- src/local/pager.nim
+++ src/local/pager.nim
@@ -2078,7 +2078,7 @@ proc updateReadLineISearch(pager: Pager; linemode: LineMode) =
       pager.container.popCursorPos()
       pager.container.clearSearchHighlights()
       pager.container.redraw = true
-      pager.isearchpromise = nil
+      pager.isearchpromise = newResolvedPromise()
     of lesEdit:
       if lineedit.news != "":
         pager.iregex = pager.compileSearchRegex(lineedit.news)
@@ -2101,7 +2101,7 @@ proc updateReadLineISearch(pager: Pager; linemode: LineMode) =
       pager.container.clearSearchHighlights()
       pager.container.sendCursorPosition()
       pager.container.redraw = true
-      pager.isearchpromise = nil
+      pager.isearchpromise = newResolvedPromise()
   )
 
 proc saveTo(pager: Pager; data: LineDataDownload; path: string) =
