29b7fa17be7ea8565e7a582f92e50e5107b8da08
bptato <nincsnevem662@gmail.com>
2024-12-15 20:02:44 +0100

    Unify assert.js for tests
    judging from the symlink, I probably meant to do this but forgot to
    finish it

--- test/asserts.js
+++ test/asserts.js
@@ -4,20 +4,26 @@ function assert(x, msg) {
 		throw new TypeError("Assertion failed" + mymsg);
 }
 
-function assert_throws(expr, error) {
+function assertThrows(expr, error) {
+	let me;
 	try {
 		eval(expr);
 	} catch (e) {
-		if (e instanceof Error)
+		if (e instanceof error)
 			return;
+		me = e;
 	}
-	throw new TypeError("Assertion failed");
+	throw new TypeError("Assertion failed: expected " + error + ", got " + me + " for expression: " + expr);
 }
 
-function assert_equals(a, b) {
+function assertEquals(a, b) {
 	assert(a === b, "Expected " + b + " but got " + a);
 }
 
-function assert_instanceof(a, b) {
+function assertNotEquals(a, b) {
+	assert(a !== b, "Expected " + b + " to have some different value");
+}
+
+function assertInstanceof(a, b) {
 	assert(a instanceof b, a + " not an instance of " + b);
 }
