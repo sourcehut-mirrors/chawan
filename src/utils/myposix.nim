d6213e64f83439d23956ef44f8e29045b2be115b
bptato <nincsnevem662@gmail.com>
2025-08-22 18:02:56 +0200

    myposix: switch getAppFilename implementation
    std's variant has several known issues across all versions, primarily
    related to error checking.
    
    Now we use QJS when possible, which appears to be more robust (although
    it supports fewer platforms).  On unsupported platforms we fall back to
    searching PATH.

--- src/utils/myposix.nim
+++ src/utils/myposix.nim
@@ -3,6 +3,10 @@
 {.push raises: [].}
 
 import std/os
+import std/posix
+import std/strutils
+
+import utils/twtstr
 
 # std's getcwd binding uses int for size, but it's size_t...
 proc my_getcwd(buf: cstring; size: csize_t): cstring {.
@@ -28,12 +32,44 @@ proc die*(s: string) {.noreturn.} =
   stderr.fwrite("cha: " & s & '\n')
   quit(1)
 
-#TODO std's implementation is a glitched mess, better rewrite it...
+proc free(p: pointer) {.importc, header: "<stdlib.h>".}
+
+proc realPath(path: string): string =
+  let p = realpath(cstring(path), nil)
+  if p == nil:
+    return ""
+  var s = $p
+  free(p)
+  move(s)
+
+# std's implementation is a glitched mess, so we don't use it.
+proc js_exepath(buffer: cstring; size: var csize_t): cint {.importc.}
+
+let PATH_MAX {.importc, header: "<limits.h>".}: csize_t
+
 proc getAppFilename*(): string =
-  result = ""
-  try:
-    result = os.getAppFilename()
-  except OSError:
-    discard
+  var s = newString(PATH_MAX)
+  var len = csize_t(s.len)
+  # let QJS do the job for us
+  # this should work on Linux and macOS
+  if js_exepath(cstring(s), len) == 0:
+    s.setLen(len)
+    return move(s)
+  # QJS died; try to find ourselves anyway
+  s = ""
+  let a0 = paramStr(0)
+  if a0.len > 0 and a0[0] == '/':
+    return realPath(a0)
+  let cwd = getcwd()
+  if '/' in a0:
+    # probably a relative path...
+    return realPath(cwd / a0)
+  # try searching PATH
+  for it in getEnvEmpty("PATH").split(':'):
+    var rp = realPath(it / a0)
+    if fileExists(rp):
+      return move(rp)
+  # welp.  return ./$0 and hope for the best
+  cwd / a0
 
 {.pop.}
