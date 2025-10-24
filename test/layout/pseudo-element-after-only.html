sourcehut-mirrors/chawan

commit f1106eb975700b1042f10fdc95da4c83914d0844

csstree, cssparser: avoid exponential behavior, parse ::backdrop

src/css/csstree.nim
@@ -85,6 +85,7 @@ type
   TreeFrame = object
     parent: Element
     computed: CSSValues
+    pseudoComputed: CSSValues
     children: seq[StyledNode]
     lastChildWasInline: bool
     captionSeen: bool
@@ -164,7 +165,12 @@ proc inheritFor(frame: TreeFrame; display: CSSDisplay): CSSValues =
 
 proc initTreeFrame(ctx: TreeContext; parent: Element; computed: CSSValues):
     TreeFrame =
-  result = TreeFrame(parent: parent, computed: computed, ctx: ctx)
+  result = TreeFrame(
+    parent: parent,
+    computed: computed,
+    pseudoComputed: computed.next,
+    ctx: ctx
+  )
 
 proc getAnonInlineComputed(frame: var TreeFrame): CSSValues =
   if frame.anonInlineComputed == nil:
@@ -360,7 +366,12 @@ proc addElement(frame: var TreeFrame; element: Element) =
     ))
 
 proc addPseudo(frame: var TreeFrame; pseudo: PseudoElement) =
-  let computed = frame.parent.getComputedStyle(pseudo)
+  var computed = frame.pseudoComputed
+  while computed != nil:
+    if computed.pseudo == pseudo:
+      break
+    computed = computed.next
+  frame.pseudoComputed = computed
   if computed != nil and computed{"display"} notin DisplayNoneLike and
       computed{"content"}.len > 0:
     frame.add(StyledNode(

src/css/cssparser.nim
@@ -307,13 +307,15 @@ type
 
   SelectorTypeNthChild = range[stNthChild..stNthLastChild]
 
+  # Note: this must match the order csstree processes pseudo-elements.
   PseudoElement* = enum
     peNone = "-cha-none"
     peBefore = "before"
-    peAfter = "after"
-    peMarker = "marker"
-    peLinkMarker = "-cha-link-marker"
     peLinkHint = "-cha-link-hint"
+    peLinkMarker = "-cha-link-marker"
+    peMarker = "marker"
+    peBackdrop = "backdrop"
+    peAfter = "after"
 
   PseudoClass* = enum
     pcFirstChild = "first-child"

commit 9e81774940c5f225f157a24ddfc8b36855666441

csstree: fix missing pseudo elements without ::before
regressed in f1106eb975

src/css/csstree.nim
@@ -369,9 +369,9 @@ proc addPseudo(frame: var TreeFrame; pseudo: PseudoElement) =
   var computed = frame.pseudoComputed
   while computed != nil:
     if computed.pseudo == pseudo:
+      frame.pseudoComputed = computed.next
       break
     computed = computed.next
-  frame.pseudoComputed = computed
   if computed != nil and computed{"display"} notin DisplayNoneLike and
       computed{"content"}.len > 0:
     frame.add(StyledNode(

test/layout/pseudo-element-after-only.expected
@@ -1,0 +1,1 @@
+testa

test/layout/pseudo-element-after-only.html
@@ -1,0 +1,5 @@
+<!DOCTYPE html>
+<style>
+div::after { content: "a" }
+</style>
+<div>test</div>