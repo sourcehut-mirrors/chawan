33023a60943ad9fdad4f1f2d67af836466f778f2
bptato <nincsnevem662@gmail.com>
2025-11-22 14:48:57 +0100

    Bump development version

--- src/main.nim
+++ src/main.nim
@@ -23,7 +23,7 @@ import utils/sandbox
 import utils/strwidth
 import utils/twtstr
 
-const ChaVersionStr0 = "Chawan browser v0.3.0"
+const ChaVersionStr0 = "Chawan browser v0.4-dev"
 
 const ChaVersionStr = block:
   var s = ChaVersionStr0 & " ("
