695dcdd587de18dd9677c77761b579b608806461
bptato <nincsnevem662@gmail.com>
2026-01-30 18:07:25 +0100

    Fix config test on FreeBSD

--- test/pager/run.sh
+++ test/pager/run.sh
@@ -3,7 +3,7 @@ if test -z "$CHA"
 then	test -f ../../cha && CHA=../../cha || CHA=cha
 fi
 
-sed -E ../../bonus/config.toml -e 's/^#([^ ])/\1/' >"${TMPDIR:-/tmp}"/config.toml
+sed -E -e 's/^#([^ ])/\1/' ../../bonus/config.toml >"${TMPDIR:-/tmp}"/config.toml
 if ! $CHA -C"${TMPDIR:-/tmp}/config.toml" /dev/null | diff /dev/null -
 then	exit 1
 fi
