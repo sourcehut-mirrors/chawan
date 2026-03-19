d0f19c4957ec93054e35c958bc165527a3a9e282
bptato <nincsnevem662@gmail.com>
2026-03-19 20:48:44 +0100

    fromjs: fix leak

--- lib/monoucha0/monoucha/fromjs.nim
+++ lib/monoucha0/monoucha/fromjs.nim
@@ -479,11 +479,12 @@ proc fromJS*(ctx: JSContext; val: JSValueConst; res: var JSArrayBufferView):
   let jsbuf = JS_GetTypedArrayBuffer(ctx, val, offset, len, bytesPerItem)
   if JS_IsException(jsbuf):
     return fjErr
-  var abuf: JSArrayBuffer
-  ?ctx.fromJSFree(jsbuf, abuf)
   if uint64(offset) + uint64(len) > uint64(int32.high):
+    JS_FreeValue(ctx, jsbuf)
     JS_ThrowRangeError(ctx, "array buffer view too large")
     return fjErr
+  var abuf: JSArrayBuffer
+  ?ctx.fromJSFree(jsbuf, abuf)
   res = JSArrayBufferView(
     abuf: abuf,
     offset: cast[int](offset),
