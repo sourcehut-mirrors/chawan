sourcehut-mirrors/chawan

commit 4414c2ff9521117c7209fb24d276ec5c6ac8a83c

Misc mailcap improvements
* add x-type, x-match, x-nc-match (from w3mmee)
* change default auto-mailcap path to $CHA_DIR/mailcap
* fix crash on headless mode + unrecognized type
* update docs

src/config/mailcap.nim
@@ -7,8 +7,10 @@ import std/posix
 
 import io/chafile
 import io/dynstream
+import monoucha/libregexp
 import types/opt
 import types/url
+import utils/lrewrap
 import utils/myposix
 import utils/twtstr
 
@@ -20,37 +22,53 @@ type
   MailcapFlag* = enum
     mfNeedsterminal = "needsterminal"
     mfCopiousoutput = "copiousoutput"
-    mfHtmloutput = "x-htmloutput" # from w3m
+    mfHtmloutput = "x-htmloutput" # w3m extension
     mfAnsioutput = "x-ansioutput" # Chawan extension
     mfSaveoutput = "x-saveoutput" # Chawan extension
     mfNeedsstyle = "x-needsstyle" # Chawan extension
     mfNeedsimage = "x-needsimage" # Chawan extension
+    mfType = "x-type" # w3mmee extension
 
-  MailcapEntry* = object
+  NamedFieldType* = enum
+    nfTest = "test"
+    nfNametemplate = "nametemplate"
+    nfEdit = "edit"
+    nfMatch = "x-match" # w3mmee extension
+    nfNcMatch = "x-nc-match" # w3mmee extension
+
+  NamedField = ref object
+    t: NamedFieldType
+    value*: string
+    next: NamedField
+
+  MailcapEntry* = object #TODO merge t and cmd?
     t*: string
     cmd*: string
     flags*: set[MailcapFlag]
-    nametemplate*: string
-    edit*: string
-    test*: string
+    fieldsHead: NamedField
 
   Mailcap* = seq[MailcapEntry]
 
-  AutoMailcap* = object
-    path*: string
-    entries*: Mailcap
+iterator fields(entry: MailcapEntry): NamedField =
+  var field = entry.fieldsHead
+  while field != nil:
+    yield field
+    field = field.next
+
+proc find*(entry: MailcapEntry; t: NamedFieldType): NamedField =
+  for field in entry.fields:
+    if field.t == t:
+      return field
+  nil
 
 proc `$`*(entry: MailcapEntry): string =
   var s = entry.t & ';' & entry.cmd
   for flag in MailcapFlag:
     if flag in entry.flags:
       s &= ';' & $flag
-  if entry.nametemplate != "":
-    s &= ";nametemplate=" & entry.nametemplate
-  if entry.edit != "":
-    s &= ";edit=" & entry.edit
-  if entry.test != "":
-    s &= ";test=" & entry.test
+  for field in entry.fields:
+    # if value is regex, then the source is until the first NUL
+    s &= ';' & $field.t & '=' & $cstring(field.value)
   s &= '\n'
   move(s)
 
@@ -104,13 +122,28 @@ proc consumeCommand(state: var MailcapParser; line: string;
     inc n
   ok(n)
 
-type NamedField = enum
-  nmTest = "test"
-  nmNametemplate = "nametemplate"
-  nmEdit = "edit"
+proc addNamedField(entry: var MailcapEntry; t: NamedFieldType;
+    fieldsTail: var NamedField; cmd: var string) =
+  var s = move(cmd)
+  if t in {nfMatch, nfNcMatch}:
+    let flags = if t == nfNcMatch: {LRE_FLAG_IGNORECASE} else: {}
+    var re: Regex
+    if not compileRegex(s, flags, re):
+      return
+    s &= '\0' & re.bytecode
+  let field = entry.find(t)
+  if field != nil:
+    field.value = move(s)
+  else:
+    let field = NamedField(t: t, value: move(s))
+    if fieldsTail == nil:
+      entry.fieldsHead = field
+    else:
+      fieldsTail.next = field
+    fieldsTail = field
 
 proc consumeField(state: var MailcapParser; line: string;
-    entry: var MailcapEntry; n: int): Opt[int] =
+    entry: var MailcapEntry; n: int; fieldsTail: var NamedField): Opt[int] =
   var n = line.skipBlanks(n)
   var s = ""
   while n < line.len:
@@ -126,11 +159,8 @@ proc consumeField(state: var MailcapParser; line: string;
       n = ?state.consumeCommand(line, cmd, n)
       while s.len > 0 and s[^1] in AsciiWhitespace:
         s.setLen(s.len - 1)
-      if x := parseEnumNoCase[NamedField](s):
-        case x
-        of nmTest: entry.test = move(cmd)
-        of nmNametemplate: entry.nametemplate = move(cmd)
-        of nmEdit: entry.edit = move(cmd)
+      if t := parseEnumNoCase[NamedFieldType](s):
+        entry.addNamedField(t, fieldsTail, cmd)
       return ok(n)
     elif c in Controls:
       return state.err("invalid character in field: " & c)
@@ -146,8 +176,9 @@ proc parseEntry*(state: var MailcapParser; line: string;
     entry: var MailcapEntry): Opt[void] =
   var n = ?state.consumeTypeField(line, entry.t)
   n = ?state.consumeCommand(line, entry.cmd, n)
+  var fieldsTail: NamedField
   while n < line.len:
-    n = ?state.consumeField(line, entry, n)
+    n = ?state.consumeField(line, entry, n, fieldsTail)
   ok()
 
 proc parseBuiltin*(mailcap: var Mailcap; buf: openArray[char]) =
@@ -160,7 +191,7 @@ proc parseBuiltin*(mailcap: var Mailcap; buf: openArray[char]) =
     doAssert res.isOk, state.error
     mailcap.add(entry)
 
-proc parseMailcap(state: var MailcapParser; mailcap: var Mailcap;
+proc parseMailcap(state: var MailcapParser; mailcap, typeMailcap: var Mailcap;
     file: ChaFile): Opt[void] =
   var line: string
   while file.readLine(line).get(false):
@@ -176,17 +207,21 @@ proc parseMailcap(state: var MailcapParser; mailcap: var Mailcap;
         break
     var entry: MailcapEntry
     ?state.parseEntry(line, entry)
-    mailcap.add(entry)
+    if mfType in entry.flags:
+      typeMailcap.add(entry)
+    else:
+      mailcap.add(entry)
     inc state.line
   return ok()
 
-proc parseMailcap*(mailcap: var Mailcap; path: string): Err[string] =
+proc parseMailcap*(mailcap, typeMailcap: var Mailcap; path: string):
+    Err[string] =
   let file0 = chafile.fopen(path, "r")
   if file0.isErr:
     return ok()
   let file = file0.get
   var state = MailcapParser(line: 1)
-  let res = state.parseMailcap(mailcap, file)
+  let res = state.parseMailcap(mailcap, typeMailcap, file)
   file.close()
   if res.isErr:
     return err(path & '(' & $state.line & "): " & msg)
@@ -287,7 +322,19 @@ proc unquoteCommand*(ecmd, contentType, outpath: string; url: URL;
       of 'u': # Netscape extension
         if url != nil: # nil in getEditorCommand
           cmd &= quoteFile($url, qs)
-      of 'd': # line; not used in mailcap, only in getEditorCommand
+      of 'h': # w3mmee extension
+        if url != nil:
+          cmd &= quoteFile(url.hostname, qs)
+      of 'H': # Chawan extension
+        if url != nil:
+          cmd &= quoteFile(url.host, qs)
+      of 'p': # w3mmee extension
+        if url != nil:
+          cmd &= quoteFile(url.port, qs)
+      of '?': # w3mmee(-ish) extension
+        if url != nil:
+          cmd &= quoteFile(url.search, qs)
+      of 'd': # Chawan extension
         if line != -1: # -1 in mailcap
           cmd &= $line
       of '{':
@@ -316,28 +363,42 @@ proc checkEntry(entry: MailcapEntry; contentType, mt, st: string; url: URL):
   if not entry.t.startsWith("*/") and not entry.t.startsWithIgnoreCase(mt) or
       not entry.t.endsWith("/*") and not entry.t.endsWithIgnoreCase(st):
     return false
-  if entry.test != "":
-    var canpipe = true
-    let cmd = unquoteCommand(entry.test, contentType, "", url, canpipe)
-    return canpipe and myposix.system(cstring(cmd)) == 0
+  for field in entry.fields:
+    case field.t
+    of nfTest:
+      var canpipe = true
+      let cmd = unquoteCommand(field.value, contentType, "", url, canpipe)
+      if canpipe and myposix.system(cstring(cmd)) == 0:
+        return false
+    of nfMatch, nfNcMatch:
+      let i = field.value.find('\0') + 1
+      let surl = $url
+      let (si, ei) = cast[REBytecode](addr field.value[i]).matchFirst(surl)
+      if si != 0 or ei != surl.len:
+        return false
+    else: discard
   true
 
 proc findPrevMailcapEntry*(mailcap: Mailcap; contentType: string; url: URL;
     last: int): int =
-  let mt = contentType.until('/') & '/'
-  let st = contentType.until(AsciiWhitespace + {';'}, mt.len - 1)
-  for i in countdown(last - 1, 0):
-    if checkEntry(mailcap[i], contentType, mt, st, url):
-      return i
+  let si = last - 1
+  if si >= 0:
+    let mt = contentType.until('/') & '/'
+    let st = contentType.until(AsciiWhitespace + {';'}, mt.len - 1)
+    for i in countdown(last - 1, 0):
+      if checkEntry(mailcap[i], contentType, mt, st, url):
+        return i
   return -1
 
 proc findMailcapEntry*(mailcap: Mailcap; contentType: string; url: URL;
     start = -1): int =
-  let mt = contentType.until('/') & '/'
-  let st = contentType.until(AsciiWhitespace + {';'}, mt.len - 1)
-  for i in start + 1 ..< mailcap.len:
-    if checkEntry(mailcap[i], contentType, mt, st, url):
-      return i
+  let si = start + 1
+  if si < mailcap.len:
+    let mt = contentType.until('/') & '/'
+    let st = contentType.until(AsciiWhitespace + {';'}, mt.len - 1)
+    for i in si ..< mailcap.len:
+      if checkEntry(mailcap[i], contentType, mt, st, url):
+        return i
   return -1
 
 proc saveEntry*(mailcap: var Mailcap; path: string; entry: MailcapEntry):

test/pager/mime.types
@@ -1,0 +1,1 @@
+test/test	test

test/pager/run.sh
@@ -8,6 +8,6 @@ if ! $CHA -C"${TMPDIR:-/tmp}/config.toml" /dev/null | diff /dev/null -
 then	exit 1
 fi
 
-$CHA -VCtest.toml >/dev/null || exit 1
+$CHA -VCtest.toml test.test >/dev/null || exit 1
 
 $CHA -r 'quit()'

test/pager/test.toml
@@ -1,3 +1,9 @@
+[buffer]
+history = false
+
+[external]
+mime-types = "mime.types"
+
   [[siteconf]] # test
  host = 'example\.org' # test
 default-headers = { # test

bonus/config.toml
@@ -36,7 +36,7 @@
 #	"/usr/etc/mailcap",
 #	"/usr/local/etc/mailcap"
 #]
-#auto-mailcap = "auto.mailcap"
+#auto-mailcap = "$CHA_DIR/mailcap"
 #mime-types = [
 #	"~/.mime.types",
 #	"/etc/mime.types",
@@ -55,7 +55,7 @@
 #cookie-file = "$CHA_DATA_DIR/cookies.txt"
 #tmpdir = "${TMPDIR:-/tmp}/cha-tmp-$LOGNAME"
 #editor = "${VISUAL:-${EDITOR:-vi}}"
-#cgi-dir = ["cgi-bin", "$CHA_LIBEXEC_DIR/cgi-bin"]
+#cgi-dir = ["$CHA_DIR/cgi-bin", "$CHA_LIBEXEC_DIR/cgi-bin"]
 #download-dir = "${TMPDIR:-/tmp}/"
 #show-download-panel = true
 #w3m-cgi-compat = false

bonus/nex
@@ -5,7 +5,7 @@ Usage (replace ~/.chawan with ~/.config/chawan if you prefer XDG):
 * put nex binary in ~/.chawan/cgi-bin
 * put in ~/.chawan/urimethodmap
 nex: /cgi-bin/nex
-* put in ~/.chawan/auto.mailcap
+* put in ~/.chawan/mailcap
 text/x-nex; "$CHA_CGI_DIR"/nex html; x-htmloutput
 * enjoy'
 

doc/cha-config.5
@@ -418,7 +418,7 @@ tmpdir
 T}@T{
 path
 T}@T{
-{usually /tmp/cha\-tmp\-user}
+{usually \(lq/tmp/cha\-tmp\-user\(rq}
 T}@T{
 Directory used to save temporary files.
 T}
@@ -428,7 +428,7 @@ editor
 T}@T{
 shell command
 T}@T{
-{usually \f[CR]$EDITOR\f[R]}
+{usually \(lq$EDITOR\(rq}
 T}@T{
 External editor command.
 %s is substituted for the file name, %d for the line number.
@@ -462,11 +462,14 @@ auto\-mailcap
 T}@T{
 path
 T}@T{
-\(lqauto.mailcap\(rq
+\(lq$CHA_DIR/mailcap\(rq
 T}@T{
 Mailcap file for entries that are automatically executed.
 .PP
 The \(lqOpen as\(rq prompt also saves entries in this file.
+.PP
+For backwards\-compatibility, if this is \(lqmailcap\(rq and the file
+does not exist, Chawan will also check \(lqauto.mailcap\(rq.
 T}
 _
 T{
@@ -550,7 +553,7 @@ bookmark
 T}@T{
 path
 T}@T{
-\(lqbookmark.md\(rq
+\(lq$CHA_DATA_DIR/bookmark.md\(rq
 T}@T{
 Path to the bookmark.md file.
 (The file it points to should have a .md extension, so that its type can
@@ -562,7 +565,7 @@ history\-file
 T}@T{
 path
 T}@T{
-\(lqhistory.uri\(rq
+\(lq$CHA_DATA_DIR/history.uri\(rq
 T}@T{
 Path to the history file.
 T}
@@ -582,7 +585,7 @@ cookie\-file
 T}@T{
 path
 T}@T{
-\(lqcookies.txt\(rq
+\(lq$CHA_DATA_DIR/cookies.txt\(rq
 T}@T{
 Path to the cookie file.
 .PP

doc/cha-mailcap.5
@@ -3,7 +3,7 @@
 .TH "CHA-MAILCAP" "5"
 .SH Mailcap
 By default, Chawan\(cqs buffers only handle HTML and plain text.
-The \f[CR]mailcap\f[R] file can be used to view other file formats using
+The \f[I]mailcap\f[R] file can be used to view other file formats using
 external commands, or to convert them to HTML/plain text before
 displaying them in Chawan.
 .PP
@@ -31,11 +31,10 @@ By default, mailcap entries are only executed if the user types
 Other options are to view the file with \f[CR]t\f[R] (text), or to save
 the file with \f[CR]s\f[R].
 .PP
-If a capital letter is typed (e.g.\ press shift and type \f[CR]R\f[R]),
-then a corresponding entry is appended to
-\f[CR]external.auto\-mailcap\f[R] (default:
-\f[CR]\(ti/.chawan/auto.mailcap\f[R], or
-\f[CR]\(ti/.config/chawan/auto.mailcap\f[R] with XDG basedirs).
+If a capital letter is typed (e.g.\ shift + \f[CR]R\f[R]), then a
+corresponding entry is appended to \f[CR]external.auto\-mailcap\f[R]
+(default: \f[CR]\(ti/.chawan/mailcap\f[R], or
+\f[CR]\(ti/.config/chawan/mailcap\f[R] with XDG basedirs).
 \f[CR](T)ext\f[R] and \f[CR](S)ave\f[R] may also be used to append
 entries corresponding to the other display options.
 .PP
@@ -46,44 +45,89 @@ to your personal mailcap file).
 Chawan adheres to the format described in RFC 1524, with a few
 extensions.
 .PP
-Note that text/html and text/plain entries are ignored.
+\f[CR]text/html\f[R] and \f[CR]text/plain\f[R] entries are ignored.
 .SS Templating
-\f[CR]%s\f[R], \f[CR]%t\f[R], and named content type fields like
-\f[CR]%{charset}\f[R] work as described in the standard.
+The command part of entries may include template strings which are
+substituted by the browser at execution.
 .PP
-If no quoting is applied, Chawan quotes the templates automatically.
+Templates do not have to be quoted; Chawan quotes them automatically.
 (This works with $(command substitutions) as well.)
 However, other software may misbehave on such templates, so it may be
-better to assign them to a variable first.
+better to assign them to a variable first, e.g.
+.IP
+.EX
+text/x\-example; s=%s cat \(dq$s\(dq; copiousoutput
+.EE
 .PP
-The non\-standard template %u may be specified to get the original URL
-of the resource.
-This is a Netscape extension that may not be compatible with other
-implementations.
-As an alternative, the \f[CR]$MAILCAP_URL\f[R] environment variable is
-set to the same value.
+Following templates are supported:
+.IP \(bu 2
+\f[CR]%s\f[R] expands to the path.
+Specifying \f[CR]%s\f[R] forces download of the external resource
+\f[I]before\f[R] the entry is executed.
+If \f[CR]%s\f[R] is not specified, the resource is instead piped to
+standard input.
+(In this case, \f[CR]needsterminal\f[R] does not apply.)
+.IP \(bu 2
+\f[CR]%t\f[R] expands to the content type.
+Named content type fields can also be specified with the syntax
+\f[CR]%{charset}\f[R].
+For example, in
+.RS 2
+.IP
+.EX
+text/html; charset=utf\-8
+.EE
+.PP
+\f[CR]%t\f[R] would expand to the above string, while
+\f[CR]%{charset}\f[R] would expand to \(lqutf\-8\(rq.
+.RE
+.IP \(bu 2
+Non\-standard templates for the resource\(cqs original URL:
+\f[CR]%u\f[R] (from Netscape) expands to the original URL of the
+resource, \f[CR]%h\f[R] (from w3mmee) expands to the hostname without
+the port, \f[CR]%H\f[R] expands to the hostname including the port, and
+\f[CR]%?\f[R] (from w3mmee) expands to the query string including the
+question mark.
+.RS 2
+.PP
+(w3mmee did not actually include the question mark in \f[CR]%?\f[R].
+However, that design could not express the difference between the empty
+query string and the null query string, so it has been changed in
+Chawan.)
+.RE
 .SS Fields
-The \f[CR]test\f[R], \f[CR]nametemplate\f[R], \f[CR]needsterminal\f[R]
-and \f[CR]copiousoutput\f[R] fields are recognized.
-The non\-standard \f[CR]x\-htmloutput\f[R], \f[CR]x\-ansioutput\f[R],
-\f[CR]x\-saveoutput\f[R] and \f[CR]x\-needsstyle\f[R] extension fields
-are also recognized.
+Following fields are recognized.
 .IP \(bu 2
 When the \f[CR]test\f[R] named field is specified, the mailcap entry is
 only used if the test command returns 0.
+For example, you can restrict entries that require X11 as follows:
 .RS 2
+.IP
+.EX
+image; feh \-; test=test \-n \(dq$DISPLAY\(dq
+.EE
 .PP
-Warning: as of now, \f[CR]%s\f[R] does not work with \f[CR]test\f[R];
+Warning: \f[CR]%s\f[R] does not work with \f[CR]test\f[R].
 \f[CR]test\f[R] named fields with a \f[CR]%s\f[R] template are skipped,
 and no data is piped into \f[CR]test\f[R] commands.
 .RE
 .IP \(bu 2
 \f[CR]copiousoutput\f[R] makes Chawan redirect the output of the
 external command\(cqs output into a new buffer.
-If either x\-htmloutput or x\-ansioutput is defined too, then it is
-ignored.
+If either \f[CR]x\-htmloutput\f[R] or \f[CR]x\-ansioutput\f[R] is
+defined too, then \f[CR]copiousoutput\f[R] is ignored.
 .IP \(bu 2
-The \f[CR]x\-htmloutput\f[R] extension field behaves the same as
+\f[CR]needsterminal\f[R] hands over control of the terminal to the
+command while it is running.
+It does nothing if one of \f[CR]copiousoutput\f[R],
+\f[CR]x\-ansioutput\f[R], \f[CR]x\-saveoutput\f[R] or
+\f[CR]x\-htmloutput\f[R] is specified.
+.IP \(bu 2
+\f[CR]nametemplate\f[R] provides a specific template for the temporary
+file created when \f[CR]%s\f[R] is specified.
+See the RFC for details.
+.IP \(bu 2
+\f[CR]x\-htmloutput\f[R] (from w3m) behaves the same as
 \f[CR]copiousoutput\f[R], but makes Chawan interpret the command\(cqs
 output as HTML.
 .IP \(bu 2
@@ -98,19 +142,45 @@ in a file.
 \f[CR]x\-needsstyle\f[R] forces CSS to be processed for the specific
 type, even if styling is disabled in the config.
 Only useful when combined with \f[CR]x\-htmloutput\f[R].
+(Also see the \f[CR]\-cha\-content\-type\f[R] media query in
+\f[B]cha\-css\f[R](7).)
 .IP \(bu 2
 \f[CR]x\-needsimage\f[R] forces images to be displayed in
 \f[CR]x\-htmloutput\f[R], even if images are disabled.
 .IP \(bu 2
-\f[CR]needsterminal\f[R] hands over control of the terminal to the
-command while it is running.
-Note: as of now, \f[CR]needsterminal\f[R] does nothing if either
-\f[CR]copiousoutput\f[R] or \f[CR]x\-htmloutput\f[R] is specified.
+\f[CR]x\-type\f[R] (from w3mmee) specifies a MIME type substitution.
+The command part is interpreted as a MIME type (without template
+expansion) which is used instead of the original type.
+Such entries are only respected in \f[CR]external.auto\-mailcap\f[R].
+.RS 2
+.PP
+\f[CR]x\-type\f[R] has a higher priority than other entries, and applies
+even to text/plain and text/html documents (which are normally excluded
+from mailcap).
+However, \f[CR]x\-type\f[R] entries do not apply if the content type was
+forced (e.g.\ using the \f[CR]\-T\f[R] flag).
+.PP
+(Note: \f[CR]x\-type\f[R] is experimental.
+Future changes to its semantics are to be expected.)
+.RE
 .IP \(bu 2
-For a description of \f[CR]nametemplate\f[R], see the RFC.
+\f[CR]x\-match\f[R] (from w3mmee) restricts an entry\(cqs URL to the
+specified regex.
+\f[CR]x\-nc\-match\f[R] is the same, but it is case\-insensitive.
+For example, \f[CR]x\-match=https?://example\(rs.org/.*\f[R] restricts
+an entry to example.org (note the backslash.)
+.RS 2
+.PP
+When one of these fields is present together with \f[CR]test\f[R], the
+result is ANDed together.
+.RE
 .SS Examples
-I recommend placing entries in \f[CR]\(ti/.chawan/auto.mailcap\f[R] (or
-\f[CR]\(ti/.config/chawan/auto.mailcap\f[R] if you use XDG basedirs).
+To automatically execute these entries, place them in
+\f[CR]\(ti/.chawan/mailcap\f[R] (or
+\f[CR]\(ti/.config/chawan/mailcap\f[R] if you use XDG basedirs).
+Alternatively, if you already have a mailcap file to share with other
+programs, you can set \f[CR]external.auto\-mailcap\f[R] to
+\f[CR]\(ti/.mailcap\f[R].
 .IP
 .EX
 # Note: these examples require an entry in mime.types that sets e.g. md as

commit 1ccca33be6e6e6251b3a5a2d6f4a17991467ce03

mailcap: fix flipped logic in test
regressed in 4414c2ff95

src/config/mailcap.nim
@@ -368,7 +368,7 @@ proc checkEntry(entry: MailcapEntry; contentType, mt, st: string; url: URL):
     of nfTest:
       var canpipe = true
       let cmd = unquoteCommand(field.value, contentType, "", url, canpipe)
-      if canpipe and myposix.system(cstring(cmd)) == 0:
+      if not canpipe or myposix.system(cstring(cmd)) != 0:
         return false
     of nfMatch, nfNcMatch:
       let i = field.value.find('\0') + 1

test/pager/mailcap
@@ -1,0 +1,2 @@
+test/test2; echo 'never used because of template'; test=echo %s\; true; copiousoutput
+test/test2; echo 'hello from mailcap'; test=true; copiousoutput

test/pager/mime.types
@@ -1,1 +1,2 @@
 test/test	test
+test/test2	test2

test/pager/run.sh
@@ -8,6 +8,10 @@ if ! $CHA -C"${TMPDIR:-/tmp}/config.toml" /dev/null | diff /dev/null -
 then	exit 1
 fi
 
-$CHA -VCtest.toml test.test >/dev/null || exit 1
+$CHA -Ctest.toml test.test >/dev/null || exit 1
+
+if ! $CHA -Ctest.toml test2.test2 | diff test2.expected -
+then	exit 1
+fi
 
 $CHA -r 'quit()'

test/pager/test.toml
@@ -3,6 +3,7 @@ history = false
 
 [external]
 mime-types = "mime.types"
+auto-mailcap = "mailcap"
 
   [[siteconf]] # test
  host = 'example\.org' # test

test/pager/test2.expected
@@ -1,0 +1,1 @@
+hello from mailcap

test/pager/test2.test2
@@ -1,0 +1,1 @@
+test