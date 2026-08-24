sourcehut-mirrors/chawan

commit 3410b74554660f33f8cdfac629d1032919c14a8a

buffer: use isSubmitButton logic in canSubmitOnClick
Makes it so that regular button types (<input type=button>,
<button type=button>) no longer automatically submit forms.

(I'm not entirely sure why I hadn't used isSubmitButton here, given that
it existed since before canSubmitOnClick.  Maybe I did it this way because
w3m does this too, but such an undocumented spec deviation is rarely a good
idea.)

src/html/dom.nim
@@ -5775,8 +5775,8 @@ proc scriptingEnabled(element: Element): bool =
   return element.asNode.document.scriptingEnabled
 
 proc isSubmitButton*(element: Element): bool =
-  if element.tagType == ttButton:
-    return element.attr(satType).equalsIgnoreCase("submit")
+  if (let element = element as HTMLButtonElement; element != nil):
+    return element.ctype == btButton
   elif (let element = element as HTMLInputElement; element != nil):
     return element.inputType in {itSubmit, itImage}
   return false

src/server/buffer.nim
@@ -211,14 +211,7 @@ proc getClickable(element: Element): Element =
 proc canSubmitOnClick(fae: FormAssociatedElement): bool =
   if fae.form == nil:
     return false
-  if fae.form.canSubmitImplicitly():
-    return true
-  if fae of HTMLButtonElement and HTMLButtonElement(fae).ctype == btSubmit:
-    return true
-  if fae of HTMLInputElement and
-      HTMLInputElement(fae).inputType in {itSubmit, itButton}:
-    return true
-  return false
+  fae.asElement.isSubmitButton() or fae.form.canSubmitImplicitly()
 
 proc getImageHover(bc: BufferContext; element: Element): string =
   if (let image = element as HTMLImageElement; image != nil):

commit fc8d85071a91e254b3a6036b8065012b5c25a502

dom: fix isSubmitButton condition
regressed in 3410b7455

src/html/dom.nim
@@ -5732,7 +5732,7 @@ proc scriptingEnabled(element: Element): bool =
 
 proc isSubmitButton*(element: Element): bool =
   if (let element = element as HTMLButtonElement; element != nil):
-    return element.ctype == btButton
+    return element.ctype == btSubmit
   elif (let element = element as HTMLInputElement; element != nil):
     return element.inputType in {itSubmit, itImage}
   return false