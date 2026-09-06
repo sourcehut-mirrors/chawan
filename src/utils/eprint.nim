aa9f10fc619f5f7e6dc60b3a3ccf159123a35aae
bptato <nincsnevem662@gmail.com>
2026-09-06 14:14:22 +0200

    eprint: trim unused procs

--- src/utils/eprint.nim
+++ src/utils/eprint.nim
@@ -23,20 +23,3 @@ when defined(release):
 else:
   func eprint*(s: varargs[string, `$`]) =
     eprint0(s)
-
-proc c_fprintf*(f: File, frmt: cstring): cint {.
-  importc: "fprintf", header: "<stdio.h>", varargs, discardable.}
-
-func elog*(s: varargs[string, `$`]) =
-  {.cast(noSideEffect), cast(tags: []), cast(raises: []).}:
-    var f: File = nil
-    if not f.open("a", fmAppend):
-      return
-    var o = ""
-    for i in 0 ..< s.len:
-      if i != 0:
-        o &= ' '
-      o &= s[i]
-    o &= '\n'
-    f.write(o)
-    close(f)
